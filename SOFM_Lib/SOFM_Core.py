e0476301a23b988395cd90f55cc1f5f36e11cbc0
Dan Griffin <unknown7979@gmail.com>
2016-04-15 12:43:40 -0400

    Initial commit. Initialized project folder with assignment pdf's, a main.py file for running the assignment code, and an SOFM library package to contain all of the SOFM code.

---
+++ SOFM_Lib/SOFM_Core.py
@@ -0,0 +1,35 @@
+'''
+Author: Daniel Griffin
+Date: 4/15/2016
+Description: This represents the core code for building and
+training SOFM nets. The code was made into a module initially
+so that it would be easier to incorporate it as an actual
+library, should the need or desire ever arise.
+'''
+
+import numpy as np
+
+
+
+
+class SOFMGrid:
+
+    def __init__(self):
+        self.width = 1
+        self.height = 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,27 @@
+'''
+Author: Daniel Griffin
+Date: 4/15/2016
+Description: This module is used to implement answers to the questions given in
+the intelligent systems class at the university of cincinnati on self organizing
+feature maps. It uses a "Library" that has all of the code related to building and
+training the SOFM core.
+'''
+
+import numpy as np
+import pandas as pd
+import scipy as scipy
+
+
+
+
+
+def main():
+    print("Running Main.")
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
